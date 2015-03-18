35089379ba4781a44e65776f6781348cb903444a
Jeff Gillis <jgillis@optoro.com>
2015-03-18 13:12:38 -0400

    Remove newrelic logic. Also move apt recipe to .kitchen.yml

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -13,7 +13,6 @@ describe 'optoro_memcached::default' do
         end
 
         %w(
-          apt
           memcached::default
           optoro_memcached::monit
           optoro_memcached::logrotate
@@ -21,52 +20,6 @@ describe 'optoro_memcached::default' do
           it 'includes #{recipe}' do
             expect(chef_run).to include_recipe(recipe)
           end
-        end
-
-        it 'installs the openjdk-7-jre-headless package' do
-          expect(chef_run).to install_package('openjdk-7-jre-headless')
-        end
-
-        it 'creates the newrelic tar file' do
-          expect(chef_run).to create_cookbook_file('/var/optoro/newrelic-npi.tar.gz').with(
-            user: 'root',
-            group: 'root',
-            mode: 0644
-          )
-        end
-
-        it 'notifies the the command to extract the tarfile' do
-          resource = chef_run.cookbook_file('/var/optoro/newrelic-npi.tar.gz')
-          expect(resource).to notify('execute[extract-newrelic-npi]').to(:run).immediately
-        end
-
-        it 'creates the newrelic plugin json template' do
-          expect(chef_run).to create_template('/var/optoro/newrelic-npi/plugins/com.newrelic.plugins.memcached/newrelic_memcached_plugin-2.0.1/config/plugin.json').with(
-            user: 'root',
-            group: 'root',
-            mode: 0644
-          )
-        end
-
-        it 'creates newrelic init script' do
-          expect(chef_run).to create_cookbook_file('/etc/init.d/newrelic_plugin_com.newrelic.plugins.memcached').with(
-            user: 'root',
-            group: 'root',
-            mode: 0755
-          )
-        end
-
-        it 'notifies the newrelic service when the init script is created' do
-          resource = chef_run.cookbook_file('/etc/init.d/newrelic_plugin_com.newrelic.plugins.memcached')
-          expect(resource).to notify('service[newrelic_plugin_com.newrelic.plugins.memcached]').to(:restart).delayed
-        end
-
-        it 'enables the newrelic service' do
-          expect(chef_run).to enable_service('newrelic_plugin_com.newrelic.plugins.memcached')
-        end
-
-        it 'starts the newrelic service' do
-          expect(chef_run).to start_service('newrelic_plugin_com.newrelic.plugins.memcached')
         end
 
         it 'creates the memcached confile' do
