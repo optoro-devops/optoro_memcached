a7888f11082fd65cd00fd2f8208fc6ce4b5d5160
Jeff Gillis <jgillis@optoro.com>
2015-03-18 17:29:13 -0400

    Make sure address is populated in the monit configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['optoro_memcached']['log_dir'] = '/var/log/memcached'
 default['optoro_memcached']['log_path'] = "#{node['optoro_memcached']['log_dir']}/memcached.log"
 default['optoro_memcached']['mem_size_mb'] = '512'
+default['monit']['address'] = 'localhost'
